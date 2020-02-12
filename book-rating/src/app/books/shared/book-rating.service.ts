3c3de7e94582ce7aea8837c197da9191f8501501
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-02-12 16:11:24 +0100

    final rating example

--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -6,13 +6,18 @@ import { Book } from './book';
 })
 export class BookRatingService {
 
-  constructor() { }
+  readonly minRating = 1;
+  readonly maxRating = 5;
 
-  rateUp(book: Book): Book {
-    return undefined;
-  }
+  rateUp = (book: Book) => ({
+    ...book,
+    rating: book.rating < this.maxRating ? book.rating + 1 : this.maxRating
+  })
 
   rateDown(book: Book): Book {
-    return undefined;
+    const rating = Math.max(book.rating - 1, this.minRating);
+    return { ...book, rating };
   }
+
+  logTitle = ({ title }: { title: string }) => console.log(title);
 }
