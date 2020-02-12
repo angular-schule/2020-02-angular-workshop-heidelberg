a0e0defb5542bf2ba9c2109ff9e58f0e699aade3
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-02-12 16:11:40 +0100

    using slice :-D

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { BookRatingService } from './shared/book-rating.service';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { BookComponent } from './book/book.component';
   ],
   exports: [
     DashboardComponent
-  ]
+  ],
+  // providers: [BookRatingService] // alte Syntax
 })
 export class BooksModule { }
--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -10,7 +11,7 @@ export class DashboardComponent implements OnInit {
 
   books: Book[];
 
-  constructor() { }
+  constructor(private br: BookRatingService) { }
 
   ngOnInit() {
     this.books = [
@@ -36,11 +37,18 @@ export class DashboardComponent implements OnInit {
   }
 
   doRateDown(book: Book) {
-
+    const ratedBook = this.br.rateDown(book);
+    this.update(ratedBook);
   }
 
   doRateUp(book: Book) {
-
+    const ratedBook = this.br.rateUp(book);
+    this.update(ratedBook);
   }
 
+  update(ratedBook: Book) {
+    this.books = this.books
+      .map(book => book.isbn === ratedBook.isbn ? ratedBook : book)
+      .sort((a, b) => b.rating - a.rating);
+  }
 }
