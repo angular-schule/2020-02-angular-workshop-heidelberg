5ee8e1897683731a06f438feb167fe872b2a43f9
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-02-14 13:54:19 +0100

    rxjs: forkJoin

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of, Observer, timer, Subscription, Observable } from 'rxjs';
+import { of, Observer, timer, Subscription, Observable, forkJoin } from 'rxjs';
 import { map, filter, reduce, mergeMap, concatMap, switchMap } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 
@@ -20,7 +20,11 @@ export class BookDetailsComponent {
 
     this.route.paramMap.pipe(
       map(paramMap => paramMap.get('isbn')),
-      switchMap(isbn => this.bs.getSingleBook(isbn))
+      switchMap(isbn => forkJoin([
+        this.bs.getSingleBook(isbn),
+        this.bs.getSingleBook(isbn)
+      ])
+      )
     ).subscribe(y => console.log(y));
   }
 }
