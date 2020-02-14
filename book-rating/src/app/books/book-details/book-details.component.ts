9e38b1bc761b6bdf96872bf7a605f49a071cc8f6
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-02-14 14:21:09 +0100

    async with if/else und as

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -16,8 +16,7 @@ export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(isbn => this.bs.getSingleBook(isbn)),
-    shareReplay(1)
+    switchMap(isbn => this.bs.getSingleBook(isbn))
   );
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) {
