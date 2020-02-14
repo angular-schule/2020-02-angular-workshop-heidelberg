0d7420557e64ab399ec98f95cbf172841ee50bb2
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-02-14 14:01:38 +0100

    async pipe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of, Observer, timer, Subscription, Observable, forkJoin } from 'rxjs';
+import { of, Observer, timer, Subscription, Observable } from 'rxjs';
 import { map, filter, reduce, mergeMap, concatMap, switchMap } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 
@@ -13,18 +13,10 @@ import { BookStoreService } from '../shared/book-store.service';
 export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
-    map(paramMap => paramMap.get('isbn'))
+    map(paramMap => paramMap.get('isbn')),
+    switchMap(isbn => this.bs.getSingleBook(isbn))
   );
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) {
-
-    this.route.paramMap.pipe(
-      map(paramMap => paramMap.get('isbn')),
-      switchMap(isbn => forkJoin([
-        this.bs.getSingleBook(isbn),
-        this.bs.getSingleBook(isbn)
-      ])
-      )
-    ).subscribe(y => console.log(y));
   }
 }
