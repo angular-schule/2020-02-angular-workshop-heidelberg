2194b3864f1d1a13c1ce8ce810a7bff091fe6813
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-02-14 14:30:01 +0100

    rxjs: ANTPATTERN, one-time only error-handling with catchError

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of, Observer, timer, Subscription, Observable } from 'rxjs';
-import { map, filter, reduce, mergeMap, concatMap, switchMap, share, shareReplay } from 'rxjs/operators';
+import { map, filter, reduce, mergeMap, concatMap, switchMap, share, shareReplay, catchError } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -16,7 +17,12 @@ export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(isbn => this.bs.getSingleBook(isbn))
+    switchMap(isbn => this.bs.getSingleBook(isbn)),
+    catchError((err: HttpErrorResponse) => of({
+      isbn: '000',
+      title: 'Fehler beim Laden von ' + err.url,
+      description: '???'
+    }))
   );
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) {
